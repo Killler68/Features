84f5ca69a83ec86d34e20c08458bae8deb8e8d5a
VIP <belevitin2010@mail.ru>
2024-09-26 01:19:14 +0300

    Create weatherPreviewBar

--- app/src/main/java/com/example/features/design/DsWeather.kt
+++ app/src/main/java/com/example/features/design/DsWeather.kt
@@ -9,14 +9,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.features.R
+import com.example.features.ui.theme.Cyan
 
 @Composable
 fun DsWeather() {
@@ -26,6 +30,7 @@ fun DsWeather() {
             .fillMaxSize()
     ) {
         DsWeatherActionBar()
+        DsWeatherPreviewBar()
     }
 
 }
@@ -59,5 +64,44 @@ fun DsWeatherActionBar() {
                     .size(50.dp)
             )
         }
+    }
+}
+
+@Composable
+fun DsWeatherPreviewBar() {
+
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+            .clip(RoundedCornerShape(20.dp))
+            .background(Cyan),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Сегодня, 26 сен. пн",
+            modifier = Modifier
+                .padding(10.dp),
+            fontSize = 20.sp,
+            color = Color.White
+        )
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "image"
+        )
+        Text(
+            text = "10 C",
+            modifier = Modifier
+                .padding(10.dp),
+            fontSize = 30.sp,
+            color = Color.White
+        )
+        Text(
+            text = "Солнечно",
+            modifier = Modifier
+                .padding(10.dp),
+            fontSize = 20.sp,
+            color = Color.White
+        )
     }
 }

a7323a9f7810e9574adaab2a7e4fd12c1fd08483
VIP <belevitin2010@mail.ru>
2024-09-26 00:59:53 +0300

    Create weatherActionBar

--- app/src/main/java/com/example/features/design/DsWeather.kt
+++ app/src/main/java/com/example/features/design/DsWeather.kt
@@ -2,15 +2,20 @@ package com.example.features.design
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.features.R
 
 @Composable
@@ -31,12 +36,28 @@ fun DsWeatherActionBar() {
     Row(
         Modifier
             .fillMaxWidth()
-            .background(Color.LightGray)
+            .background(Color.LightGray),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(text = "Буденновск")
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_foreground),
-            contentDescription = "image"
+        Text(
+            text = "Буденновск",
+            modifier = Modifier
+                .padding(start = 10.dp)
+
         )
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp),
+            contentAlignment = Alignment.CenterEnd
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "image",
+                modifier = Modifier
+                    .size(50.dp)
+            )
+        }
     }
 }

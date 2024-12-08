888f73a19b9ba6932a8cf0d63354249143de04b4
VIP <belevitin2010@mail.ru>
2024-12-08 08:43:06 +0300

    Create drawer to features

--- app/src/main/java/com/example/features/features/design/DsFeatures.kt
+++ app/src/main/java/com/example/features/features/design/DsFeatures.kt
@@ -14,21 +14,27 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.features.MainScreenEvent
-import com.example.features.MainViewModel
 import com.example.features.R
 import com.example.features.common.extension.getRawNameFeaturesCityEngToRuExtension
 import com.example.features.common.viewmodel.SharedViewModel
@@ -37,12 +43,12 @@ import com.example.features.navigation.Screens
 import com.example.features.ui.theme.Gray
 import com.example.features.ui.theme.LightGray
 import com.example.features.weather.viewmodel.WeatherViewModel
+import kotlinx.coroutines.launch
 import org.koin.androidx.compose.getViewModel
 
 @Composable
 fun DsFeatures(navController: NavController) {
 
-    val mainViewModel: MainViewModel = viewModel()
     val featuresViewModel: FeaturesViewModel = getViewModel()
 
     val weatherViewModel: WeatherViewModel = getViewModel()
@@ -54,71 +60,103 @@ fun DsFeatures(navController: NavController) {
     val sharedViewModel: SharedViewModel = getViewModel()
     val user by sharedViewModel.currentUser.collectAsState()
 
-    Column(
-        Modifier
-            .fillMaxSize()
-            .background(Color.White)
-    ) {
-        Row(
-            Modifier
-                .fillMaxWidth()
-                .background(Gray)
 
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.ic_launcher_foreground),
-                contentDescription = "image_menu",
-                modifier = Modifier
-                    .padding(start = 10.dp)
-                    .clip(CircleShape)
-                    .size(40.dp)
-                    .clickable {
-                        navController.navigate(Screens.Profile.route)
-                        mainViewModel.handleEvent(MainScreenEvent.NavigateToWeather)
-                    }
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .size(40.dp),
-                contentAlignment = Alignment.CenterStart
-            ) {
-                user?.login?.let {
-                    Text(
-                        text = it,
-                        modifier = Modifier
-                            .padding(horizontal = 20.dp)
-                            .fillMaxWidth(),
-                        maxLines = 1
+    val items = listOf("Профиль", "Настройки", "Настройки 2")
+    val selectedItem = remember { mutableStateOf(items[0]) }
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                items.forEach { item ->
+                    TextButton(
+                        onClick = {
+                            scope.launch { drawerState.close() }
+                            selectedItem.value = item
+
+                            when (selectedItem.value) {
+                                "Профиль" -> navController.navigate(Screens.Profile.route)
+                                "Настройки" -> navController.navigate(Screens.Registration.route)
+                            }
+                        },
                     )
-                } ?: Text("No user logged in")
-
+                    { Text(text = item, fontSize = 20.sp) }
+                }
             }
-        }
+        },
+        content = {
 
-        LazyRow(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight()
-                .background(LightGray)
-        ) {
-            itemsIndexed(featuresViewModel.loadFeatures()) { index, feature ->
-                DsFeatureItems(feature) {
-                    if (index == 1) {
-                        feature.title =
-                            "В ${
-                                weatherViewModel.previewBarWeather.value.city
-                                    .getRawNameFeaturesCityEngToRuExtension()
-                            } сегодня"
-                        feature.description = "${weatherViewModel.previewBarWeather.value.temp} C"
-                        feature.image = weatherViewModel.previewBarWeather.value.icon
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .background(Color.White)
+            ) {
+                Row(
+                    Modifier
+                        .fillMaxWidth()
+                        .background(Gray)
+
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                        contentDescription = "image_menu",
+                        modifier = Modifier
+                            .padding(start = 10.dp)
+                            .clip(CircleShape)
+                            .size(40.dp)
+                            .clickable {
+                                scope.launch { drawerState.open() }
+                            }
+                    )
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .size(40.dp),
+                        contentAlignment = Alignment.CenterStart
+                    ) {
+                        user?.login?.let {
+                            Text(
+                                text = it,
+                                modifier = Modifier
+                                    .padding(horizontal = 20.dp)
+                                    .fillMaxWidth(),
+                                maxLines = 1
+                            )
+                        } ?: Text("No user logged in")
+
                     }
-                    navController.navigate(feature.feature)
                 }
 
+
+                LazyRow(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()
+                        .background(LightGray)
+                ) {
+                    itemsIndexed(featuresViewModel.loadFeatures()) { index, feature ->
+                        DsFeatureItems(feature) {
+                            if (index == 1) {
+                                feature.title =
+                                    "В ${
+                                        weatherViewModel.previewBarWeather.value.city
+                                            .getRawNameFeaturesCityEngToRuExtension()
+                                    } сегодня"
+                                feature.description =
+                                    "${weatherViewModel.previewBarWeather.value.temp} C"
+                                feature.image = weatherViewModel.previewBarWeather.value.icon
+                            }
+                            navController.navigate(feature.feature)
+                        }
+
+                    }
+                }
             }
         }
-    }
+    )
 }
+
 
 

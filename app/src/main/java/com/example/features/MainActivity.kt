0aac241bc1a9acaffd7c85254e1283d2acede8d0
VIP <belevitin2010@mail.ru>
2025-01-13 04:53:56 +0300

    Create start screen and add lightGray

--- app/src/main/java/com/example/features/MainActivity.kt
+++ app/src/main/java/com/example/features/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.features
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.material3.Text
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.example.features.navigation.NavigationAppHost
+import com.example.features.start.StartScreen
+import kotlinx.coroutines.delay
 import org.koin.androidx.compose.getViewModel
 
 class MainActivity : ComponentActivity() {
@@ -17,13 +21,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             val viewModel: MainViewModel = getViewModel()
             val localeState = viewModel.locale.collectAsState(initial = "")
-            if (localeState.value.isNotEmpty()) {
-                NavigationAppHost(checkLocale = localeState.value)
-            } else {
-                Text("Loading...")
-            }
+            var showStartScreen by remember { mutableStateOf(true) }
+
             LaunchedEffect(Unit) {
+                delay(1000L)
+                showStartScreen = false
                 viewModel.checkLocale()
+            }
+
+            if (showStartScreen) {
+                StartScreen()
+            } else if (localeState.value.isNotEmpty()) {
+                NavigationAppHost(checkLocale = localeState.value)
             }
         }
     }

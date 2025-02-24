4b5cf155fe7e71c778aeb06de56567823f338154
VIP <belevitin2010@mail.ru>
2025-02-25 01:03:55 +0300

    Transiotion constants api to package strings

--- app/src/main/java/com/example/features/common/api/WeatherApi.kt
+++ app/src/main/java/com/example/features/common/api/WeatherApi.kt
@@ -1,7 +1,6 @@
 package com.example.features.common.api
 
 import WeatherResponse
-import androidx.compose.runtime.MutableState
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
@@ -11,7 +10,7 @@ const val WEATHER = "https://api.openweathermap.org/"
 const val API_KEY = "1fb564b0448c20cff8c8a08d408dba5b"
 
 interface WeatherApi {
-    @GET("data/2.5/forecast?&appid=1fb564b0448c20cff8c8a08d408dba5b&units=metric")
+    @GET("data/2.5/forecast?&appid=1fb564b0448c20cff8c8a08d408dba5b&units=metric&lang=ru")
     suspend fun getWeather(
         @Query("q") city: String
     ): WeatherResponse

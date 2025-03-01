f8a316172bb165b88d5482b5bada8efd93b696c0
VIP <belevitin2010@mail.ru>
2025-03-02 01:32:08 +0300

    Fix weatherUseCase

--- app/src/main/java/com/example/features/weather/usecase/WeatherUseCaseImpl.kt
+++ app/src/main/java/com/example/features/weather/usecase/WeatherUseCaseImpl.kt
@@ -1,25 +1,25 @@
 package com.example.features.weather.usecase
 
-import com.example.features.common.extension.dateFormatUnixTime
 import com.example.features.weather.model.WeatherData
 import com.example.features.weather.model.WeatherWeek
 import com.example.features.weather.model.toWeatherData
 import com.example.features.weather.viewmodel.WeatherUseCase
-import kotlinx.coroutines.coroutineScope
+
 class WeatherUseCaseImpl(
     private val repository: WeatherRepository
 ) : WeatherUseCase {
 
-    private val weatherDays = mutableMapOf<String, MutableList<WeatherWeek>>()
+    override suspend operator fun invoke(city: String): List<WeatherData> {
+        val weatherWeek: List<WeatherWeek> = repository.getWeatherWeek(city)
 
-    override suspend fun invoke(city: String): List<WeatherData> = coroutineScope {
-        repository.getWeatherWeek(city).forEach {
-            if (weatherDays[it.day.dateFormatUnixTime()] != null)
-                weatherDays[it.day.dateFormatUnixTime()]?.add(it)
-            else weatherDays[it.day.dateFormatUnixTime()] = mutableListOf(it)
-        }
-        val weatherDataWithOutLastElement = weatherDays.toWeatherData().toMutableList()
-        weatherDataWithOutLastElement.removeLast()
-        weatherDataWithOutLastElement
+        if (weatherWeek.isEmpty()) return emptyList()
+
+        val weatherDays =
+            weatherWeek.groupBy { it.dtText.substring(0, 10) }
+                .mapValues { (_, data) ->
+                    data.sortedBy { it.day }
+                }
+
+        return weatherDays.toWeatherData()
     }
 }

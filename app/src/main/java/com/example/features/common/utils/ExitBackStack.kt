0127e9e935bd2ca51ec934375a1f0f2e8be5af30
VIP <belevitin2010@mail.ru>
2025-03-11 04:22:10 +0300

    Поменял Toast на AlertDialog с двумя кнопками отменить и подтвердить которая выходит из приложения

--- app/src/main/java/com/example/features/common/utils/ExitBackStack.kt
+++ app/src/main/java/com/example/features/common/utils/ExitBackStack.kt
@@ -1,15 +1,23 @@
 package com.example.features.common.utils
 
 import android.content.Intent
-import android.widget.Toast
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import com.example.features.ui.theme.Cyan
 import kotlinx.coroutines.delay
 
 @Composable
@@ -17,22 +25,61 @@ fun ExitBackStack() {
     var exit by remember { mutableStateOf(false) }
     val context = LocalContext.current
 
-    LaunchedEffect(key1 = exit) {
+    LaunchedEffect(exit) {
         if (exit) {
-            delay(2000)
+            delay(4000)
             exit = false
         }
     }
 
     BackHandler(enabled = true) {
-        if (exit) {
-            context.startActivity(Intent(Intent.ACTION_MAIN).apply {
-                addCategory(Intent.CATEGORY_HOME)
-                flags = Intent.FLAG_ACTIVITY_NEW_TASK
-            })
-        } else {
-            exit = true
-            Toast.makeText(context, "Нажмите еще раз для выхода", Toast.LENGTH_SHORT).show()
-        }
+        exit = true
+    }
+
+    if (exit) {
+        AlertDialog(
+            onDismissRequest = { exit = false },
+            title = {
+                Text(
+                    text = "Выйти из приложения?",
+                    fontSize = 16.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+            },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        context.startActivity(Intent(Intent.ACTION_MAIN).apply {
+                            addCategory(Intent.CATEGORY_HOME)
+                            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                        })
+                        exit = false
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Cyan
+                    )
+                ) {
+                    Text(
+                        text = "Подтвердить",
+                        fontSize = 12.sp
+                    )
+                }
+            },
+            dismissButton = {
+                Button(
+                    onClick = { exit = false },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Cyan
+                    )
+                ) {
+                    Text(
+                        text = "Отмена",
+                        fontSize = 12.sp
+                    )
+                }
+            }
+        )
     }
 }
